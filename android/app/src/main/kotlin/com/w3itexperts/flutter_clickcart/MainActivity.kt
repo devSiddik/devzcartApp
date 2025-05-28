3999b0d7b7c9ed7c203fe004e800fe46cf2b3917
devsiddik <abubokor.diu@gmail.com>
2025-05-28 10:43:26 +0600

    remove unnecessary files

--- android/app/src/main/kotlin/com/w3itexperts/flutter_clickcart/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.w3itexperts.flutter_clickcart
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/helper/helper_functions.dart
+++
@@ -1,5 +0,0 @@
-class IKHelpers {
-  IKHelpers._();
-
-  
-}
